cdf866fa97cffe1bbc3443554dd977ca77e8228c
Eser Ozvataf <eser@sent.com>
2014-08-07 03:36:46 +0300

    * added navigation menu, thanks to samet aydemir.

--- assets/js/larukedi-github-io.js
+++ assets/js/larukedi-github-io.js
@@ -12394,7 +12394,10 @@ if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
 
 })(this.laroux);
 ;$l.ready(function() {
-	var bodyJqueryWrapper = $(document.body);
+    var bodyJqueryWrapper = $(document.body);
+    var pageElement = $l('#page');
+    var sidebarElement = $l('#sidebar');
+    var sidebarStatus = false;
 
     $l.dom.setEvent(
         $l(['.xscroll-link']),
@@ -12412,36 +12415,52 @@ if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
         }
     );
 
-	$('#p-filter a').click(function() {
+    $l.dom.setEvent(
+        $l(['.sidebar-toggle-link']),
+        'click',
+        function(ev, elem) {
+            if (!sidebarStatus) {
+                sidebarStatus = true;
+                $l.css.addClass(pageElement, 'with-sidebar');
+                $l.css.addClass(sidebarElement, 'visible');
+            } else {
+                sidebarStatus = false;
+                $l.css.removeClass(pageElement, 'with-sidebar');
+                $l.css.removeClass(sidebarElement, 'visible');
+            }
+        }
+    );
+
+    $('#p-filter a').click(function() {
         var that = $(this);
-		$('#p-filter a.btn-act').removeClass('btn-act');
-		that.addClass('btn-act');
+        $('#p-filter a.btn-act').removeClass('btn-act');
+        that.addClass('btn-act');
 
-		var p_filter = that.attr('rel');
+        var p_filter = that.attr('rel');
 
-		$('.p-item').each(function() {
+        $('.p-item').each(function() {
             var that2 = $(this);
 
-			var p_property = that2.attr('rel');
-			if (p_property == p_filter && p_filter != 'all') {
-				that2.fadeTo(300, 1);
-				$('img', this).fadeTo(300, 1);
-				that2.removeClass('item-off');
-			} else if (p_filter != 'all') {
-				that2.fadeTo(300, 0.3);
-				$('img', this).fadeTo(300, 0.2);
-				that2.addClass('item-off');
-			} else {
-				that2.fadeTo(300, 1);
-				$('img', this).fadeTo(300, 1);
-				that2.removeClass('item-off');
-			}
-		});
+            var p_property = that2.attr('rel');
+            if (p_property == p_filter && p_filter != 'all') {
+                that2.fadeTo(300, 1);
+                $('img', this).fadeTo(300, 1);
+                that2.removeClass('item-off');
+            } else if (p_filter != 'all') {
+                that2.fadeTo(300, 0.3);
+                $('img', this).fadeTo(300, 0.2);
+                that2.addClass('item-off');
+            } else {
+                that2.fadeTo(300, 1);
+                $('img', this).fadeTo(300, 1);
+                that2.removeClass('item-off');
+            }
+        });
         
         return false;
-	});
+    });
 
-	$('.showmore').click(function() {
+    $('.showmore').click(function() {
         var that = $(this);
         
         $('.' + that.attr('rel')).fadeIn();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 $l.ready(function() {
-	var bodyJqueryWrapper = $(document.body);
+    var bodyJqueryWrapper = $(document.body);
+    var pageElement = $l('#page');
+    var sidebarElement = $l('#sidebar');
+    var sidebarStatus = false;
 
     $l.dom.setEvent(
         $l(['.xscroll-link']),
@@ -17,36 +20,52 @@ $l.ready(function() {
         }
     );
 
-	$('#p-filter a').click(function() {
+    $l.dom.setEvent(
+        $l(['.sidebar-toggle-link']),
+        'click',
+        function(ev, elem) {
+            if (!sidebarStatus) {
+                sidebarStatus = true;
+                $l.css.addClass(pageElement, 'with-sidebar');
+                $l.css.addClass(sidebarElement, 'visible');
+            } else {
+                sidebarStatus = false;
+                $l.css.removeClass(pageElement, 'with-sidebar');
+                $l.css.removeClass(sidebarElement, 'visible');
+            }
+        }
+    );
+
+    $('#p-filter a').click(function() {
         var that = $(this);
-		$('#p-filter a.btn-act').removeClass('btn-act');
-		that.addClass('btn-act');
+        $('#p-filter a.btn-act').removeClass('btn-act');
+        that.addClass('btn-act');
 
-		var p_filter = that.attr('rel');
+        var p_filter = that.attr('rel');
 
-		$('.p-item').each(function() {
+        $('.p-item').each(function() {
             var that2 = $(this);
 
-			var p_property = that2.attr('rel');
-			if (p_property == p_filter && p_filter != 'all') {
-				that2.fadeTo(300, 1);
-				$('img', this).fadeTo(300, 1);
-				that2.removeClass('item-off');
-			} else if (p_filter != 'all') {
-				that2.fadeTo(300, 0.3);
-				$('img', this).fadeTo(300, 0.2);
-				that2.addClass('item-off');
-			} else {
-				that2.fadeTo(300, 1);
-				$('img', this).fadeTo(300, 1);
-				that2.removeClass('item-off');
-			}
-		});
+            var p_property = that2.attr('rel');
+            if (p_property == p_filter && p_filter != 'all') {
+                that2.fadeTo(300, 1);
+                $('img', this).fadeTo(300, 1);
+                that2.removeClass('item-off');
+            } else if (p_filter != 'all') {
+                that2.fadeTo(300, 0.3);
+                $('img', this).fadeTo(300, 0.2);
+                that2.addClass('item-off');
+            } else {
+                that2.fadeTo(300, 1);
+                $('img', this).fadeTo(300, 1);
+                that2.removeClass('item-off');
+            }
+        });
         
         return false;
-	});
+    });
 
-	$('.showmore').click(function() {
+    $('.showmore').click(function() {
         var that = $(this);
         
         $('.' + that.attr('rel')).fadeIn();
